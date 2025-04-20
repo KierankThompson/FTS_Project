c2d7215c70809326c15af0004427db7ffa1cbd68
KierankThompson <thompson.kierank@gmail.com>
2025-04-20 00:57:38 -0400

    fixed backtest

--- backtest.py
+++ backtest.py
@@ -1,16 +1,31 @@
 import pandas as pd
+import datetime
 
 
-def backtest(startDate,endDate,startingMoney=1000000):
-    allocations = {'JPM':0.33,'WMT':0.33,'AVGO':0.33}
+def backtest(startDate,endDate,startingMoney=1000000,vwapLag=14):
+    print("loading dataframes...")
+    allocations = {'JPM':int(startingMoney/3),'WMT':int(startingMoney/3),'AVGO':int(startingMoney/3)}
     positions = {'JPM':0,"WMT":0,"AVGO":0}
     curMoney = startingMoney
     jpmDF = pd.read_csv('JPM cleaned.csv')
     wmtDF = pd.read_csv('WMT cleaned.csv')
     avgoDF = pd.read_csv('AVGO cleaned.csv')
-    print("loading dataframes...")
-    startDT = startDate + ' ' + '13:30:00+00:00'
+    jpmDF['day'] = pd.to_datetime(jpmDF['ts_event']).dt.date
+    jpmDF['year_month'] = pd.to_datetime(jpmDF['ts_event']).dt.strftime('%Y-%m')
+    jpmDF.set_index('ts_event', inplace=True)
+    wmtDF['day'] = pd.to_datetime(wmtDF['ts_event']).dt.date
+    wmtDF['year_month'] = pd.to_datetime(wmtDF['ts_event']).dt.strftime('%Y-%m')
+    wmtDF.set_index('ts_event', inplace=True)
+    avgoDF['day'] = pd.to_datetime(avgoDF['ts_event']).dt.date
+    avgoDF['year_month'] = pd.to_datetime(avgoDF['ts_event']).dt.strftime('%Y-%m')
+    avgoDF.set_index('ts_event', inplace=True)
+    dataframes = {'JPM':jpmDF,"WMT":wmtDF,"AVGO":avgoDF}
 
+    all_days = jpmDF['day'].unique()
+    all_months = jpmDF['year_month'].unique()
+       
+    startDT = startDate + ' ' + '13:30:00+00:00'
+    
     jpmStart = jpmDF.index[jpmDF['ts_event'] == startDT][0]
     wmtStart = wmtDF.index[wmtDF['ts_event'] == startDT][0]
     avgoStart = avgoDF.index[avgoDF['ts_event'] == startDT][0]
@@ -18,37 +33,9 @@ def backtest(startDate,endDate,startingMoney=1000000):
         raise Exception("CSV's out of order")
     i = jpmStart
     day = '2018-09-24'
-    while jpmDF.loc[i,'ts_event'].split()[0] != endDate:
-        #jpm first
-        jpmDate, jpmTime = jpmDF.loc[i,'ts_event']
-        jpmOpen = jpmDF.loc[i,'open']
-        jpmHigh = jpmDF.loc[i,'high']
-        jpmLow = jpmDF.loc[i,'low']
-        jpmClose = jpmDF.loc[i,'close']
-        jpmVol = jpmDF.loc[i,'volume']
-
-        wmtDate, wmtTime = wmtDF.loc[i, 'ts_event']
-        wmtOpen = wmtDF.loc[i, 'open']
-        wmtHigh = wmtDF.loc[i, 'high']
-        wmtLow = wmtDF.loc[i, 'low']
-        wmtClose = wmtDF.loc[i, 'close']
-        wmtVol = wmtDF.loc[i, 'volume']
-
-        avgoDate, avgoTime = avgoDF.loc[i, 'ts_event']
-        avgoOpen = avgoDF.loc[i, 'open']
-        avgoHigh = avgoDF.loc[i, 'high']
-        avgoLow = avgoDF.loc[i, 'low']
-        avgoClose = avgoDF.loc[i, 'close']
-        avgoVol = avgoDF.loc[i, 'volume']
-
-        if jpmDate != day:
-            jpmDayOpen = jpmOpen
-            wmtDayOpen = avgoOpen
-            avgoDayOpen = jpmOpen
-            jpmDayClose = jpmDF.loc[i-1,'close']
-            wmtDayClose = wmtDF.loc[i-1,'close']
-            avgoDayClose = avgoDF.loc[i-1,'close']
-           
+    
+    while jpmDF.loc[i,'ts_event'].split()[0] <= endDate:
+        
             
         
         
@@ -58,4 +45,4 @@ def backtest(startDate,endDate,startingMoney=1000000):
     
     
     
-backtest('2018-09-25',10)
+backtest('2018-09-25','2018-09-29')
